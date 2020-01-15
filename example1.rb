06c79ab8ac5f6ce6c05a18c805dae26f3e71a770
Paul Yeo <paulyeo21@gmail.com>
2020-01-15 08:27:01 -0800

    Finish first example

--- example1.rb
+++ example1.rb
@@ -30,10 +30,56 @@ end
 def doubleThread
   s1 = SumUp.new(1, MAX_NUM / 2)
   s2 = SumUp.new(MAX_NUM / 2 + 1, MAX_NUM)
+
+  t1 = Thread.new do
+    s1.add()
+  end
+
+  t2 = Thread.new do
+    s2.add()
+  end
+
+  t1.join()
+  t2.join()
+  s1.current + s2.current
+end
+
+def doubleProcesses
+  r1, w1 = IO.pipe
+  r2, w2 = IO.pipe
+
+  pid1 = Process.fork do
+    s = SumUp.new(1, MAX_NUM / 2)
+    s.add()
+    w1.puts s.current
+  end
+
+  pid2 = Process.fork do
+    s = SumUp.new(MAX_NUM / 2 + 1, MAX_NUM)
+    s.add()
+    w2.puts s.current
+  end
+
+  Process.wait pid1
+  Process.wait pid2
+
+  w1.close()
+  w2.close()
+  r1.read().to_i + r2.read().to_i
 end
 
 starting = Process.clock_gettime(Process::CLOCK_MONOTONIC)
 sum = singleThread()
 ending = Process.clock_gettime(Process::CLOCK_MONOTONIC)
 puts "Single thread: #{ending - starting} to sum up to #{sum}"
+
+starting = Process.clock_gettime(Process::CLOCK_MONOTONIC)
+sum = doubleThread()
+ending = Process.clock_gettime(Process::CLOCK_MONOTONIC)
+puts "Double thread: #{ending - starting} to sum up to #{sum}"
+
+starting = Process.clock_gettime(Process::CLOCK_MONOTONIC)
+sum = doubleProcesses()
+ending = Process.clock_gettime(Process::CLOCK_MONOTONIC)
+puts "Double processes: #{ending - starting} to sum up to #{sum}"
 
